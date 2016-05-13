ffc13b781496d7470d8cbe485d0f42ab5e855e55
Mikko Heikkinen <hawk@biomi.org>
2016-05-13 19:18:31 +0300

    UI files, based on suomenlinnut, tweaks for this application

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Löydä lähin lintutorni</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/app.css">
+
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <div id="header-container">
+            <header class="wrapper clearfix">
+                <h2 class="title">
+                    <a href="./" id="titlelink">
+                        <img src="images/cygcyg.svg.php" id="logo" alt="">
+                        Löydä lähin lintutorni
+                    </a>
+                    <a id="helplink" href="#" title="Apua ja taustatietoa">?</a>
+                </h2>
+            </header>
+        </div>
+
+
+        <div id="help-container">
+            <p>Introteksti
+            </p>
+
+        </div>
+
+        <div id="error-container"></div>
+        <div id="message-container"></div>
+
+        <div id="main-container">
+        </div> <!-- #main-container -->
+
+        <div id="share-container">
+            <div class="fb-share-button" data-href="http://www.biomi.org/" data-layout="button_count"></div>
+            <a href="https://twitter.com/share" class="twitter-share-button" data-lang="fi" data-dnt="true">Twiittaa</a>
+<script>/*
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+*/</script>
+        </div>
+
+        <div id="footer-container">
+            <footer class="wrapper">
+
+
+                <h3>Tietolähteet</h3>
+
+
+
+                <p id="credits">Toteutus: <strong>Mikko Heikkinen / <a href="http://www.biomi.org/">biomi.org</a></strong> | <a href="https://github.com/mikkohei13/retkikohteet">Code on Github</a></p>
+
+
+
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script>
+            var logData = { };
+            <?php
+                echo "logData.datetime = " . date("YmdHis") . ";\n";
+                echo "logData.ip = \"" . $_SERVER['REMOTE_ADDR'] . "\";\n";
+
+
+                // Automatic base url, https://gist.github.com/mikkohei13/9312936
+                $base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+                $base_url .= "://".$_SERVER['HTTP_HOST'];
+                $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+                echo "var rootUrl = \"" . $base_url . "\";\n" // http://192.168.56.10/suomen-linnut/
+
+            ?>
+        </script>
+        <script src="js/main.js"></script>
+
+        <div id="fb-root"></div>
+        <script>
+/*
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+*/
+        </script>
+
+
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <?php // include_once "../../googleanalytics.html"; ?>
+
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,110 @@
+
+// Based on code by Ian Devlin
+// http://html5doctor.com/finding-your-position-with-geolocation/
+
+document.addEventListener("DOMContentLoaded", determineLocation);
+
+function determineLocation(event)
+{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      handlePosition, 
+      displayError,
+      {
+        enableHighAccuracy: false,
+        timeout: 30000, // ms
+        maximumAge: 60000 // ms
+      }
+    );
+    $( "#error-container" ).html( "<div>Sijaintiasi haetaan...</div>" );  
+  }
+  else {
+    $( "#error-container" ).html( "<div>Selaimesi ei valitettavasti tue paikannusta.</div>" );
+    console.log("navigator.geolocation not supported");
+
+    logData.error = "not supported";
+//    logger();
+
+  }
+
+  function handlePosition(position) {
+    if (position.coords.accuracy > 500)
+    {
+      $( "#message-container" ).html( "<div>Tarkkaa sijaintiasi ei saatu selville, joten etäisyydet torneihin eivät ole tarkkoja. Jos käytät tietokonetta, kokeile mielummin älypuhelimella jossa on GPS! <span class='additional'>(virhesäde " + position.coords.accuracy + " m)</span></div>" );
+
+      logData.error = "inaccurate";
+    }
+
+    logData.accuracy = position.coords.accuracy;
+    logData.altitude = position.coords.altitude;
+    logData.altitudeAccuracy = position.coords.altitudeAccuracy;
+    logData.latitude = position.coords.latitude;
+    logData.longitude = position.coords.longitude;
+
+    $.getJSON(
+      (rootUrl + "/api/towers/?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude),
+      updatePage
+    );
+
+    console.log(position);
+  }
+
+  // This is the meat
+  function updatePage(towersJSON)
+  {
+      console.log(towersJSON);
+      console.log("Success!");
+      $( "#error-container" ).html("");
+//      $( "#main-container" ).load( "allspecies.php?grid=" + towersJSON.N + ":" + towersJSON.E );
+
+//      logData.N = towersJSON.N;
+//      logData.E = towersJSON.E;
+      logData.error = "success";
+//      logger();
+  }
+
+  function displayError(error) {
+    var errors = { 
+      1: 'Olet kieltänyt paikannustiedon käytön selaimessa tai käyttöjärjestelmässä. Salli se ja yritä uudelleen.',
+      2: 'Sijaintia ei pystytty hakemaan. Siirry paikkaan, josta on esteettömämpi näkymä taivaalle.',
+      3: 'Sijainnin hakeminen kesti liian kauan. Tarkista että puhelimesi paikannustoiminto on päällä.'
+    };
+    console.log(errors[error.code]);
+    $( "#error-container" ).html("<div>" + errors[error.code] + "</div>");
+
+    logData.error = "error " + error.code;
+//    logger();
+  }
+
+  /*
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+  */
+}
+
+function logger()
+{
+  logData.userAgent = navigator.userAgent;
+  logData.innerHeight = window.innerHeight;
+  logData.innerWidth = window.innerWidth;
+
+//  console.log(navigator);
+//  console.log(window);
+
+/*
+  $.post( "logger.php", logData, function( loggerResponse ) {
+    console.log(loggerResponse);
+  });
+*/
+}
+
+// Help link
+$( "#helplink" ).click(function() {
+
+  $( "#help-container" ).slideToggle( 200, function() {
+    console.log("Help slided open");
+  });
+
+
+});
